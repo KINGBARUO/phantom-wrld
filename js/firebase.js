f9ddf57d1f50a4f1296ac193d982b86e820439ee
KINGBARUO <braunbreaker2000@gmail.com>
2026-08-01 18:01:23 +0100

    Export auth from Firebase initialization

--- js/firebase.js
+++ js/firebase.js
@@ -20,7 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+export const auth = getAuth(app)
 
 // Sign Up
 window.signUp = function () {
